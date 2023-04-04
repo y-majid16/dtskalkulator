b71d095d81a565515cf702697ea293457948262a
y-majid16 <yaumulmajid@gmail.com>
2023-04-04 09:45:22 +0700

    add JS

---
+++ main.js
@@ -0,0 +1,17 @@
+const calculatorScreen = document.querySelector(".calculator-screen");
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number;
+};
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    updateScreen(event.target.value);
+  });
+});
+
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
