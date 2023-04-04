ee1fb2b73d4a7194625af393d82f77ca218410c5
y-majid16 <yaumulmajid@gmail.com>
2023-04-04 10:05:43 +0700

    update function calculate

--- main.js
+++ main.js
@@ -8,10 +8,63 @@ const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    updateScreen(event.target.value);
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
   });
 });
 
 let prevNumber = "";
 let calculationOperator = "";
 let currentNumber = "0";
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber;
+  calculationOperator = operator;
+  currentNumber = "";
+};
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updateScreen(currentNumber);
+});
+
+const calculate = () => {
+  let result = "";
+  switch (calculationOperator) {
+    case "+":
+      result = prevNumber + currentNumber;
+      break;
+    case "-":
+      result = prevNumber - currentNumber;
+      break;
+    case "*":
+      result = prevNumber * currentNumber;
+      break;
+    case "/":
+      result = prevNumber / currentNumber;
+      break;
+
+    default:
+      return;
+  }
+  currentNumber = result;
+  calculationOperator = "";
+};
